4dfd3dc4e900d0e97719d6e3646e0e3c4572331b
ferchorduy <53240305+ferchorduy@users.noreply.github.com>
2026-05-31 21:46:18 -0400

    Finish sepia

--- Week-4-Memory/filter-less/helpers.c
+++ Week-4-Memory/filter-less/helpers.c
@@ -1,4 +1,7 @@
 #include "helpers.h"
+#include <math.h>
+
+int sepia_check(int sepia_value);
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
@@ -7,10 +10,10 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-            int average = (image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed) / 3;
-            image[i][j].rgbtBlue = average;
-            image[i][j].rgbtGreen = average;
-            image[i][j].rgbtRed = average;
+            int average = (image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed) / 3.0;
+            image[i][j].rgbtBlue = round(average);
+            image[i][j].rgbtGreen = round(average);
+            image[i][j].rgbtRed = round(average);
         }
     }
     return;
@@ -19,17 +22,52 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            float sepiaBlue = (0.272 * image[i][j].rgbtRed) + (0.534 * image[i][j].rgbtGreen) + (0.131 * image[i][j].rgbtBlue);
+            float sepiaGreen = (0.349 * image[i][j].rgbtRed) + (0.686 * image[i][j].rgbtGreen) + (0.168 * image[i][j].rgbtBlue);
+            float sepiaRed = (0.393 * image[i][j].rgbtRed) + (0.769 * image[i][j].rgbtGreen) + (0.189 * image[i][j].rgbtBlue);
+            image[i][j].rgbtBlue = sepia_check(round(sepiaBlue));
+            image[i][j].rgbtGreen = sepia_check(round(sepiaGreen));
+            image[i][j].rgbtRed = sepia_check(round(sepiaRed));
+        }
+    }
     return;
 }
 
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            
+        }
+    }
     return;
 }
 
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            
+        }
+    }
     return;
+}
+
+// Checks if a sepia color conversion results in a value higher than the max limit of 255
+int sepia_check(int sepia_value)
+{
+    if (sepia_value >= 255) return 255;
+    return sepia_value;
 }
