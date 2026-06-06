801aeaa5e08e66407753aaf28f20f335268f46f7
ferchorduy <53240305+ferchorduy@users.noreply.github.com>
2026-06-06 01:10:33 -0400

    Finish password.c

--- Week-2-Arrays/password/password.c
+++ Week-2-Arrays/password/password.c
@@ -1,9 +1,9 @@
 // Check that a password has at least one lowercase letter, uppercase letter, number and symbol
-// Practice iterating through a string
-// Practice using the ctype library
 
 #include <stdio.h>
 #include <stdbool.h>
+#include <ctype.h>
+#include <string.h>
 
 bool valid(char *password);
 
@@ -26,6 +26,18 @@ int main(void)
 // TODO: Complete the Boolean function below
 bool valid(char *password)
 {
-    
-    return false;
+    bool upper = false;
+    bool lower = false;
+    bool number = false;
+    bool symbol = false;
+
+    for (int i = 0; i < strlen(password); i++)
+    {
+        if (isupper(password[i])) upper = true;
+        else if (islower(password[i])) lower = true;
+        else if (isdigit(password[i])) number = true;
+        else if (!isalnum(password[i])) symbol = true;
+    }
+
+    return upper && lower && number && symbol;
 }
