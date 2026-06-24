6e835abfbfd645bac716ad4868e4b2c457f0e21f
ferchorduy <53240305+ferchorduy@users.noreply.github.com>
2026-06-24 19:20:41 -0400

    Add OrduyLap

---
+++ Week-10-Final-Project/app.py
@@ -0,0 +1,13 @@
+import sqlite3
+
+from flask import Flask, request, render_template, redirect, session
+from flask_session import Session
+from werkzeug.security import check_password_hash, generate_password_hash
+
+
+app = Flask(__name__)
+
+app.config["SESSION_PERMAMENT"] = False
+app.confif["SESSION_TYPE"] = "filesystem"
+Session(app)
+
