ad2ccd154e620dc44caf51992fb991e5d8e0e0ce
ferchorduy <53240305+ferchorduy@users.noreply.github.com>
2026-06-26 14:54:13 -0400

    Added timezone parameter to to_local()

--- Week-9-Flask/finance/helpers.py
+++ Week-9-Flask/finance/helpers.py
@@ -107,8 +107,8 @@ def validate_card_number(card_number):
     else: return False
 
 
-def to_local(utc_timestamp_str):
+def to_local(utc_timestamp_str, timezone="America/New_York"):
     utc_dt = datetime.strptime(utc_timestamp_str, "%Y-%m-%d %H:%M:%S")
     utc_dt = pytz.utc.localize(utc_dt)
-    local_dt = utc_dt.astimezone(pytz.timezone("America/New_York"))
+    local_dt = utc_dt.astimezone(pytz.timezone(timezone))
     return local_dt.strftime("%Y-%m-%d %I:%M %p")
