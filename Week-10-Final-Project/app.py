43129fe68782c4596bd65e43ea95f1ed5d7b3741
ferchorduy <53240305+ferchorduy@users.noreply.github.com>
2026-06-26 03:47:14 -0400

    Finish register and sqlite3 usage of cursor

--- Week-10-Final-Project/app.py
+++ Week-10-Final-Project/app.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-from flask import Flask, flash, request, render_template, redirect, session
+from flask import Flask, flash, request, render_template, redirect, session, g
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
 
@@ -9,17 +9,27 @@ from helpers import apology, login_required, to_local
 # Configure application
 app = Flask(__name__)
 
+# Timestamp filter
+app.jinja_env.filters["to_local"] = to_local
+
 # Configure session to use filesystem (instead of signed cookies)
-app.config["SESSION_PERMAMENT"] = False
+app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # SQLite connection
-orduylap = sqlite3.connect("orduylap.db")
-orduylap_db = orduylap.cursor()
+def get_db():
+    if "db" not in g:
+        g.db = sqlite3.connect("orduylap.db")
+        g.db.row_factory = sqlite3.Row
+    return g.db
 
-# Timestamp filter
-app.jinja_env.filters["to_local"] = to_local
+@app.teardown_appcontext
+def close_db(exception):
+    db = g.pop("db", None)
+    if db is not None:
+        db.close()
+
 
 @app.after_request
 def after_request(response):
@@ -58,10 +68,39 @@ def records():
     return apology("To do")
 
 
-@app.route("/register")
+@app.route("/register", methods=["GET", "POST"])
 def register():
-    """ """
-    return apology("To do")
+    """Register a new user"""
+
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        confirmation = request.form.get("confirmation")
+
+        if not username:
+            return apology("PLEASE INPUT A USERNAME!")
+        
+        if not password:
+            return apology("PLEASE INPUT A PASSWORD!")
+        
+        if not confirmation:
+            return apology("PLEASE CONFIRM PASSWORD!")
+        
+        if password != confirmation:
+            return apology("PASSWORDS DO NOT MATCH!")
+        
+        try:
+            orduylap = get_db()
+            orduylap_db = orduylap.cursor()
+            orduylap_db.execute("INSERT INTO users (username, hash) VALUES(?, ?)", (username, generate_password_hash(password)))
+            orduylap.commit()
+            flash("Registered!")
+            return redirect("/login")
+        except sqlite3.IntegrityError:
+            return apology("USERNAME EXISTS!")
+        
+    else:
+        return render_template("register.html")
 
 
 @app.route("/login", methods=["GET", "POST"])
@@ -82,9 +121,11 @@ def login():
             return apology("Must provide password", 403)
         
         # Query database for username
+        orduylap = get_db()
+        orduylap_db = orduylap.cursor()
         rows = orduylap_db.execute(
-            "SELECT username FROM users WHERE username = ?", username
-        )
+            "SELECT * FROM users WHERE username = ?", (username,)
+        ).fetchall()
 
         # Ensure username exists and password is correct
         if len(rows) != 1 or not check_password_hash(rows[0]["hash"], password):
